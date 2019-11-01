93e88c42928c6eb02cb2ea4bedac69af4b82f69c
minthet naing <itonemm@gmail.com>
2019-11-01 17:14:09 +0630

    modified delete movie function

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { NzDrawerService, NzMessageService } from 'ng-zorro-antd';
-import { MovieFormComponent } from '../movie-form/movie-form.component';
-import { MovieId, MovieService } from 'src/app/services/movie.service';
+import { Component, OnInit } from "@angular/core";
+import { NzDrawerService, NzMessageService } from "ng-zorro-antd";
+import { MovieFormComponent } from "../movie-form/movie-form.component";
+import { MovieId, MovieService } from "src/app/services/movie.service";
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.less']
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.less"]
 })
 export class MovieListComponent implements OnInit {
-
   isSpinning = false;
   movies: MovieId[] = [];
   filterMovies: MovieId[] = [];
@@ -17,34 +16,33 @@ export class MovieListComponent implements OnInit {
   constructor(
     private drawer: NzDrawerService,
     private message: NzMessageService,
-    private movieService: MovieService,
-  ) { }
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
     this.isSpinning = true;
-    this.movieService.getAll()
-      .subscribe(
-        movies => { 
-          this.movies = movies;
-          this.filterMovies = [...movies];
-          this.isSpinning = false;
-        },
-        err => console.log(err)
-      );
+    this.movieService.getAll().subscribe(
+      movies => {
+        this.movies = movies;
+        this.filterMovies = [...movies];
+        this.isSpinning = false;
+      },
+      err => console.log(err)
+    );
   }
 
   showAddNew() {
     this.drawer.create({
-      nzTitle: 'Add New Movie',
+      nzTitle: "Add New Movie",
       nzWidth: 400,
       nzMaskClosable: false,
-      nzContent: MovieFormComponent,
+      nzContent: MovieFormComponent
     });
   }
 
   showEdit(movie: MovieId) {
     this.drawer.create({
-      nzTitle: 'Edit Movie',
+      nzTitle: "Edit Movie",
       nzWidth: 400,
       nzMaskClosable: false,
       nzContent: MovieFormComponent,
@@ -57,25 +55,25 @@ export class MovieListComponent implements OnInit {
   delete(movie: MovieId) {
     this.isSpinning = true;
 
-    this.movieService.delete(movie)
-      .then(
-        () => {
-          this.message.success(`Movie '${movie.title}' deleted successfully.'`);
-          this.isSpinning = false;
-        }
-      )
-      .catch(
-        err => {
-          this.message.error('Error occured!');
-          console.log(err);
-        }
-      );
+    this.movieService
+      .delete(movie.id)
+      .then(() => {
+        this.message.success(`Movie '${movie.title}' deleted successfully.'`);
+        this.isSpinning = false;
+      })
+      .catch(err => {
+        this.message.error("Error occured!");
+        console.log(err);
+      });
   }
 
   search(value: string) {
     const search = value.trim();
     const movies = [...this.movies];
-    this.filterMovies = search ? movies.filter(movie => movie.title.toLowerCase().indexOf(search.toLowerCase()) != -1) : movies;
+    this.filterMovies = search
+      ? movies.filter(
+          movie => movie.title.toLowerCase().indexOf(search.toLowerCase()) != -1
+        )
+      : movies;
   }
-
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,56 +1,60 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
-import { shareReplay, map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestoreCollection,
+  AngularFirestore
+} from "@angular/fire/firestore";
+import { shareReplay, map } from "rxjs/operators";
 
 export interface Movie {
-    title: string;
-    languageId: string;
-    language: string;
-    playListId: string;
-    playListTitle: string;
-    categoryId: string;
-    categoryName: string;
-    imageFileName?: string;
-    videoFileName?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+  title: string;
+  languageId: string;
+  language: string;
+  playListId: string;
+  playListTitle: string;
+  categoryId: string;
+  categoryName: string;
+  imageFileName?: string;
+  videoFileName?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-export interface MovieId extends Movie { id: string; }
+export interface MovieId extends Movie {
+  id: string;
+}
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root"
 })
 export class MovieService {
+  private moviesCollection: AngularFirestoreCollection<Movie>;
 
-    private moviesCollection: AngularFirestoreCollection<Movie>;
+  constructor(afs: AngularFirestore) {
+    this.moviesCollection = afs.collection("movies");
+  }
 
-    constructor(afs: AngularFirestore) {
-        this.moviesCollection = afs.collection('movies');
-    }
+  getAll() {
+    return this.moviesCollection.snapshotChanges().pipe(
+      map(actions =>
+        actions.map(a => {
+          const data = a.payload.doc.data() as Movie;
+          const id = a.payload.doc.id;
+          return { id, ...data } as MovieId;
+        })
+      ),
+      shareReplay()
+    );
+  }
 
-    getAll() {
-        return this.moviesCollection.snapshotChanges()
-            .pipe(
-                map(actions => actions.map(a => {
-                    const data = a.payload.doc.data() as Movie;
-                    const id = a.payload.doc.id;
-                    return { id, ...data } as MovieId;
-                })),
-                shareReplay()
-            );
-    }
+  save(movie: Movie) {
+    return this.moviesCollection.add(movie);
+  }
 
-    save(movie: Movie) {
-        return this.moviesCollection.add(movie);
-    }
+  update(movieId: string, movie: Movie) {
+    return this.moviesCollection.doc(movieId).update(movie);
+  }
 
-    update(movieId: string, movie: Movie) {
-        return this.moviesCollection.doc(movieId).update(movie);
-    } 
-
-    delete(movie: MovieId) {
-        return this.moviesCollection.doc(movie.id).delete();
-    }
-
+  delete(movieId: string) {
+    return this.moviesCollection.doc(movieId).delete();
+  }
 }
